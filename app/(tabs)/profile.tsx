d05b2adc6d40d288dfc30d7c5258efc6545a5767
Luke Haas <luke.haas@scsstudent.org>
2025-11-10 11:24:47 -0800

    feat: Complete video 5

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,12 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 import { theme } from "@/theme";
+import { useUserStore } from "@/store/userStore";
 
 export default function ProfileScreen() {
+  const toggleHasOnboarded = useUserStore((store) => store.toggleHadOnboarded);
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Profile</Text>
+      <Button title="Back to onboarding" onPress={toggleHasOnboarded} />
     </View>
   );
 }
@@ -14,8 +16,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     backgroundColor: theme.colorWhite,
-  },
-  text: {
-    fontSize: 24,
   },
 });
