d62f01692926e2f12e0a2c43d6f9df0eeb7dac08
Luke Haas <luke.haas@scsstudent.org>
2025-11-12 20:42:19 -0800

    feat: Complete video 12

--- theme.ts
+++ theme.ts
@@ -5,5 +5,7 @@ export const theme = {
   colorAppleGreen: "#a0d36c",
   colorLimeGreen: "#d0e57e",
   colorWhite: "#fff",
+  colorBlack: "#000",
+  colorGrey: "#808080",
   colorLightGrey: "#eee",
 };
